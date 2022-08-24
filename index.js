8a9ae23dc316cf491b58f9af44711637df0f8eee
Maureen Murugi <maureen.murugi@student.moringaschool.com>
2022-08-24 18:10:16 +0300

    commit tree 122cbdc303d8de745a7247caaaccae11528697c5

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Your code goes here
+document.addEventListener('DOMContentLoaded', function() {
+    document.querySelector('#text').textContent = 'This is really cool!'
+
+    
+})
+console.log('This console.log() fires when the index.js loads before DOMContentLoader');
